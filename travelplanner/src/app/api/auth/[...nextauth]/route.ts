72534febb37a44c4467e2eaecc1ea0493e06d936
RicardoMartinsGithub <ricardofilipe.martins@my.istec.pt>
2025-10-15 16:16:06 +0100

    fix

--- travelplanner/src/app/api/auth/[...nextauth]/route.ts
+++ travelplanner/src/app/api/auth/[...nextauth]/route.ts
@@ -59,7 +59,7 @@ const handler = NextAuth({
     },
     async session({ session, token }) {
       if (token && session.user) {
-        (session.user as any).id = token.id as string;
+        session.user.id = token.id as string;
       }
       return session;
     },
